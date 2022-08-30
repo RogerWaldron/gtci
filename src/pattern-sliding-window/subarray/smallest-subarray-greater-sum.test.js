4116c40c4fafd85d2e63bf888cf0a61ccb53e548
Roger Waldron <roger.waldron@gmail.com>
2022-08-30 23:03:26 +0200

    test: :white_check_mark: fixed input for second test

--- src/pattern-sliding-window/subarray/smallest-subarray-greater-sum.test.js
+++ src/pattern-sliding-window/subarray/smallest-subarray-greater-sum.test.js
@@ -1,20 +1,21 @@
 const brute = require("./smallest-subarray-greater-sum");
 const better = require("./smallest-subarray-greater-sum");
 
-test("Example 1 - Expect Brute and Better results to be equal", () => {
+test("Example 1 - Expect Brute and Better to return 2", () => {
   const arr = [2, 1, 5, 2, 3, 2];
   const S = 7;
-  const answer = 2;
   const result = [5, 2];
+  const answer = 2;
 
   let bruteResult = brute(arr, S);
   let betterResult = better(arr, S);
 
-  expect(bruteResult).toStrictEqual(betterResult);
+  expect(bruteResult).toEqual(answer);
+  // expect(bruteResult).toStrictEqual(betterResult);
 });
 
-test("Example 2 - Expect Brute and Better results to be equal", () => {
-  const arr = [2, 1, 5, 2, 3, 2];
+test("Example 2 - Expect Brute and Better to return 1", () => {
+  const arr = [2, 1, 5, 2, 8];
   const S = 7;
   const result = [8];
   const answer = 1;
@@ -22,10 +23,11 @@ test("Example 2 - Expect Brute and Better results to be equal", () => {
   let bruteResult = brute(arr, S);
   let betterResult = better(arr, S);
 
-  expect(bruteResult).toStrictEqual(betterResult);
+  expect(bruteResult).toEqual(answer);
+  // expect(bruteResult).toStrictEqual(betterResult);
 });
 
-test("Example 3 - Expect Brute and Better results to be equal", () => {
+test("Example 3 - Expect Brute and Better to return 3", () => {
   const arr = [3, 4, 1, 1, 6];
   const S = 8;
   const result = [3, 4, 1] || [1, 1, 6];
@@ -34,5 +36,6 @@ test("Example 3 - Expect Brute and Better results to be equal", () => {
   let bruteResult = brute(arr, S);
   let betterResult = better(arr, S);
 
-  expect(bruteResult).toStrictEqual(betterResult);
+  expect(bruteResult).toEqual(answer);
+  // expect(bruteResult).toStrictEqual(betterResult);
 });
