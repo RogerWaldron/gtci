941c8a86e1d5d065b80aa8954be3572c6e61ca05
Roger Waldron <roger.waldron@gmail.com>
2022-08-30 23:05:20 +0200

    refactor: brute force solution Smallest Subarray With a Greater Sum

--- src/pattern-sliding-window/subarray/smallest-subarray-greater-sum.js
+++ src/pattern-sliding-window/subarray/smallest-subarray-greater-sum.js
@@ -2,6 +2,22 @@
 Given an array of positive integers and a number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
 */
 
-const brute = () => {};
+const brute = (arr, S) => {
+  let minLength = Number.POSITIVE_INFINITY;
+  let windowSum = 0;
+  let windowStart = 0;
 
-const better = () => {};
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+    while (windowSum >= S) {
+      minLength = Math.min(minLength, windowEnd - windowStart + 1);
+      windowSum -= arr[windowStart];
+      windowStart++;
+    }
+  }
+  return minLength === Number.POSITIVE_INFINITY ? 0 : minLength;
+};
+
+const better = (arr, S) => {};
+
+(module.exports = brute), better;
